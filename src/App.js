f6afc7ab06d1c12e81fe77986fc13c5b46ad8588
Shishir Tiwari <shishir456tiwari@gmail.com>
2021-10-02 10:59:13 +0530

    Show Hide Feature Added For Nav Btn

--- src/App.js
+++ src/App.js
@@ -1,64 +1,78 @@
-import './App.css';
-import { Carousel } from './lib';
+import "./App.css";
+import { Carousel } from "./lib";
 
 function App() {
   const data = [
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/1200px-GoldenGateBridge-001.jpg",
       caption: `<div>
         San Francisco<br/><span>Next line</span>
-      </div>`
+      </div>`,
     },
     {
-      image: "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
-      caption: "<div>San Francisco</div>"
+      image:
+        "https://cdn.britannica.com/s:800x450,c:crop/35/204435-138-2F2B745A/Time-lapse-hyper-lapse-Isle-Skye-Scotland.jpg",
+      caption: "<div>San Francisco</div>",
     },
     {
-      image: "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
-      caption: "<div>San Francisco</div>"
+      image:
+        "https://static2.tripoto.com/media/filter/tst/img/735873/TripDocument/1537686560_1537686557954.jpg",
+      caption: "<div>San Francisco</div>",
     },
     {
-      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
-      caption: "<div>San Francisco</div>"
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Palace_of_Fine_Arts_%2816794p%29.jpg/1200px-Palace_of_Fine_Arts_%2816794p%29.jpg",
+      caption: "<div>San Francisco</div>",
     },
     {
-      image: "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-      caption: "<div>San Francisco</div>"
+      image:
+        "https://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
+      caption: "<div>San Francisco</div>",
     },
     {
-      image: "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
-      caption: "Darjeeling"
+      image:
+        "https://www.tusktravel.com/blog/wp-content/uploads/2020/07/Best-Time-to-Visit-Darjeeling-for-Honeymoon.jpg",
+      caption: "Darjeeling",
     },
     {
-      image: "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
-      caption: "<div>San Francisco</div>"
+      image:
+        "https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx",
+      caption: "<div>San Francisco</div>",
     },
     {
-      image: "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
-      caption: "<div>San Francisco</div>"
+      image:
+        "https://images.ctfassets.net/bth3mlrehms2/6Ypj2Qd3m3jQk6ygmpsNAM/61d2f8cb9f939beed918971b9bc59bcd/Scotland.jpg?w=750&h=422&fl=progressive&q=50&fm=jpg",
+      caption: "<div>San Francisco</div>",
     },
     {
-      image: "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
-      caption: "<div>San Francisco</div>"
-    }
+      image:
+        "https://www.oyorooms.com/travel-guide/wp-content/uploads/2019/02/summer-7.jpg",
+      caption: "<div>San Francisco</div>",
+    },
   ];
 
   const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 'bold',
-  }
+    fontSize: "2em",
+    fontWeight: "bold",
+  };
   const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 'bold',
-  }
+    fontSize: "20px",
+    fontWeight: "bold",
+  };
   return (
     <div className="App">
       <div style={{ textAlign: "center" }}>
         <h2>React Carousel Minimal</h2>
-        <p>Easy to use, responsive and customizable carousel component for React Projects.</p>
-        <div style={{
-          padding: "0 20px"
-        }}>
+        <p>
+          Easy to use, responsive and customizable carousel component for React
+          Projects.
+        </p>
+        <div
+          style={{
+            padding: "0 20px",
+          }}
+        >
           <Carousel
             data={data}
             time={2000}
@@ -77,6 +91,7 @@ function App() {
             slideImageFit="cover"
             thumbnails={true}
             thumbnailWidth="100px"
+            showNavBtn={false}
             style={{
               textAlign: "center",
               maxWidth: "850px",
--- src/lib/components/Carousel.js
+++ src/lib/components/Carousel.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import Swipe from 'react-easy-swipe';
-import './styles/index.css';
+import React, { useEffect, useState } from "react";
+import Swipe from "react-easy-swipe";
+import "./styles/index.css";
 
 function Carousel({
   data,
@@ -20,9 +20,9 @@ function Carousel({
   slideBackgroundColor,
   slideImageFit,
   thumbnails,
-  thumbnailWidth
+  thumbnailWidth,
+  showNavBtn = true,
 }) {
-
   //Initialize States
   const [slide, setSlide] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
@@ -30,32 +30,30 @@ function Carousel({
 
   //Function to change slide
   const addSlide = (n) => {
-    if (slide + n >= data.length)
-      setSlide(0);
-    else if (slide + n < 0)
-      setSlide(data.length - 1);
-    else
-      setSlide(slide + n);
-  }
+    if (slide + n >= data.length) setSlide(0);
+    else if (slide + n < 0) setSlide(data.length - 1);
+    else setSlide(slide + n);
+  };
 
   //Start the automatic change of slide
   useEffect(() => {
     if (automatic) {
       var index = slide;
-      const interval = setInterval(() => {
-        if (!isPaused) {
-
-          setSlide(index);
-          index++;
-          if (index >= data.length)
-            index = 0;
-          if (index < 0)
-            index = data.length - 1;
-        }
-      }, time ? time : 2000);
-      return () => { clearInterval(interval); };
+      const interval = setInterval(
+        () => {
+          if (!isPaused) {
+            setSlide(index);
+            index++;
+            if (index >= data.length) index = 0;
+            if (index < 0) index = data.length - 1;
+          }
+        },
+        time ? time : 2000
+      );
+      return () => {
+        clearInterval(interval);
+      };
     }
-
   }, [isPaused, change]);
 
   function scrollTo(el) {
@@ -64,9 +62,12 @@ function Carousel({
 
     // check if element not in view
     if (elLeft >= elParentLeft + el.parentNode.scrollLeft) {
-      el.parentNode.scroll({ left: elLeft - elParentLeft, behavior: 'smooth' })
+      el.parentNode.scroll({ left: elLeft - elParentLeft, behavior: "smooth" });
     } else if (elLeft <= el.parentNode.offsetLeft + el.parentNode.scrollLeft) {
-      el.parentNode.scroll({ left: el.offsetLeft - el.parentNode.offsetLeft, behavior: 'smooth' })
+      el.parentNode.scroll({
+        left: el.offsetLeft - el.parentNode.offsetLeft,
+        behavior: "smooth",
+      });
     }
   }
 
@@ -74,7 +75,6 @@ function Carousel({
   useEffect(() => {
     var slides = document.getElementsByClassName("carousel-item");
     var dots = document.getElementsByClassName("dot");
-
 
     var slideIndex = slide;
     var i;
@@ -88,7 +88,10 @@ function Carousel({
     if (thumbnails) {
       var thumbnailsArray = document.getElementsByClassName("thumbnail");
       for (i = 0; i < thumbnailsArray.length; i++) {
-        thumbnailsArray[i].className = thumbnailsArray[i].className.replace(" active-thumbnail", "");
+        thumbnailsArray[i].className = thumbnailsArray[i].className.replace(
+          " active-thumbnail",
+          ""
+        );
       }
       if (thumbnailsArray[slideIndex] !== undefined)
         thumbnailsArray[slideIndex].className += " active-thumbnail";
@@ -97,120 +100,166 @@ function Carousel({
 
     if (slides[slideIndex] !== undefined)
       slides[slideIndex].style.display = "block";
-    if (dots[slideIndex] !== undefined)
-      dots[slideIndex].className += " active";
+    if (dots[slideIndex] !== undefined) dots[slideIndex].className += " active";
   }, [slide, isPaused]);
 
   return (
     <div style={style} className="box">
-      <div style={{
-        maxWidth: width ? width : "600px",
-        maxHeight: height ? height : "400px",
-      }}
+      <div
+        style={{
+          maxWidth: width ? width : "600px",
+          maxHeight: height ? height : "400px",
+        }}
       >
         <Swipe
-          onSwipeRight={() => { addSlide(-1); setChange(!change) }}
-          onSwipeLeft={() => { addSlide(1); setChange(!change) }}
+          onSwipeRight={() => {
+            addSlide(-1);
+            setChange(!change);
+          }}
+          onSwipeLeft={() => {
+            addSlide(1);
+            setChange(!change);
+          }}
         >
           <div
             className="carousel-container"
             style={{
               maxWidth: width ? width : "600px",
               height: height ? height : "400px",
-              backgroundColor: slideBackgroundColor ? slideBackgroundColor : "darkgrey",
+              backgroundColor: slideBackgroundColor
+                ? slideBackgroundColor
+                : "darkgrey",
               borderRadius: radius,
             }}
           >
-            {
-              data.map((item, index) => {
-                return (
-                  <div
-                    className="carousel-item fade"
+            {data.map((item, index) => {
+              return (
+                <div
+                  className="carousel-item fade"
+                  style={{
+                    maxWidth: width ? width : "600px",
+                    maxHeight: height ? height : "400px",
+                  }}
+                  onMouseDown={(e) => {
+                    automatic && setIsPaused(true);
+                  }}
+                  onMouseUp={(e) => {
+                    automatic && setIsPaused(false);
+                  }}
+                  onMouseLeave={(e) => {
+                    automatic && setIsPaused(false);
+                  }}
+                  onTouchStart={(e) => {
+                    automatic && setIsPaused(true);
+                  }}
+                  onTouchEnd={(e) => {
+                    automatic && setIsPaused(false);
+                  }}
+                  key={index}
+                >
+                  {slideNumber && (
+                    <div className="slide-number" style={slideNumberStyle}>
+                      {index + 1} / {data.length}
+                    </div>
+                  )}
+                  <img
+                    src={item.image}
+                    alt={item.caption}
+                    className="carousel-image"
                     style={{
-                      maxWidth: width ? width : "600px",
-                      maxHeight: height ? height : "400px"
-                    }}
-                    onMouseDown={e => {
-                      automatic &&
-                        setIsPaused(true);
-                    }}
-                    onMouseUp={e => {
-                      automatic &&
-                        setIsPaused(false);
-                    }}
-                    onMouseLeave={e => {
-                      automatic &&
-                        setIsPaused(false);
-                    }}
-                    onTouchStart={e => {
-                      automatic &&
-                        setIsPaused(true);
-                    }}
-                    onTouchEnd={e => {
-                      automatic &&
-                        setIsPaused(false);
-                    }}
-                    key={index}>
-                    {slideNumber &&
-                      <div className="slide-number" style={slideNumberStyle}>{index + 1} / {data.length}</div>
-                    }
-                    <img src={item.image} alt={item.caption} className="carousel-image" style={{
                       borderRadius: radius,
                       objectFit: slideImageFit ? slideImageFit : "cover",
                     }}
-                    />
-                    {isPaused &&
-                      <div className="pause-icon pause" style={{
+                  />
+                  {isPaused && (
+                    <div
+                      className="pause-icon pause"
+                      style={{
                         color: pauseIconColor ? pauseIconColor : "white",
-                        fontSize: pauseIconSize ? pauseIconSize : "40px"
-                      }}>II</div>}
-                    <div className={`carousel-caption-${captionPosition ? captionPosition : "bottom"}`} style={captionStyle} dangerouslySetInnerHTML={{ __html: item.caption }}></div>
+                        fontSize: pauseIconSize ? pauseIconSize : "40px",
+                      }}
+                    >
+                      II
+                    </div>
+                  )}
+                  <div
+                    className={`carousel-caption-${
+                      captionPosition ? captionPosition : "bottom"
+                    }`}
+                    style={captionStyle}
+                    dangerouslySetInnerHTML={{ __html: item.caption }}
+                  ></div>
+                </div>
+              );
+            })}
 
-                  </div>
-                );
-              })
-            }
-
-            <a className="prev" onClick={(e) => { addSlide(-1); setChange(!change) }}>&#10094;</a>
-            <a className="next" onClick={(e) => { addSlide(1); setChange(!change) }}>&#10095;</a>
-            {dots &&
+            {showNavBtn && (
+              <a
+                className="prev"
+                onClick={(e) => {
+                  addSlide(-1);
+                  setChange(!change);
+                }}
+              >
+                &#10094;
+              </a>
+            )}
+            {showNavBtn && (
+              <a
+                className="next"
+                onClick={(e) => {
+                  addSlide(1);
+                  setChange(!change);
+                }}
+              >
+                &#10095;
+              </a>
+            )}
+            {dots && (
               <div className="dots">
-                {
-                  data.map((item, index) => {
-                    return (
-                      <span className="dot" key={index} onClick={(e) => { setSlide(index); setChange(!change); }}></span>
-                    );
-                  })
-                }
+                {data.map((item, index) => {
+                  return (
+                    <span
+                      className="dot"
+                      key={index}
+                      onClick={(e) => {
+                        setSlide(index);
+                        setChange(!change);
+                      }}
+                    ></span>
+                  );
+                })}
               </div>
-            }
+            )}
           </div>
-
         </Swipe>
-
-
       </div>
-      {thumbnails &&
-        <div className="thumbnails" id="thumbnail-div" style={{ maxWidth: width }}>
-          {
-            data.map((item, index) => {
-              return (
-                <img
-                  width={thumbnailWidth ? thumbnailWidth : "100px"}
-                  src={item.image}
-                  alt={item.caption}
-                  className="thumbnail"
-                  id={`thumbnail-${index}`}
-                  key={index}
-                  onClick={(e) => { setSlide(index); setChange(!change) }}
-                />
-              )
-            })
-          }
+      {thumbnails && (
+        <div
+          className="thumbnails"
+          id="thumbnail-div"
+          style={{ maxWidth: width }}
+        >
+          {data.map((item, index) => {
+            return (
+              <img
+                width={thumbnailWidth ? thumbnailWidth : "100px"}
+                src={item.image}
+                alt={item.caption}
+                className="thumbnail"
+                id={`thumbnail-${index}`}
+                key={index}
+                onClick={(e) => {
+                  setSlide(index);
+                  setChange(!change);
+                }}
+              />
+            );
+          })}
         </div>
-      }
-    </div >
-  )
+      )}
+    </div>
+  );
 }
 
-export default Carousel
+export default Carousel;
